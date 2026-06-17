c6212c882607257bbe87d186e9d7d0e66e3d2186
ajayganuga <ajayganuga@gmail.com>
2026-06-17 23:46:49 +0530

    VPC

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  
+   backend "s3" {
+     bucket = "32-tf-remote-state-dev"
+     key    = "expense-dev-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+     region = "us-east-1"
+     dynamodb_table = "32-tf-remote-state-dev"
+   }
+ }
+ 
+
+provider "aws" {
+  region = "us-east-1"
+}
